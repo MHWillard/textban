c8a846b31f43ca3e454607092478730ed4198256
MHWillard <mhwillard@gmail.com>
2024-12-17 15:23:25 -0500

    initial

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace textban.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,14 @@
+using System.Linq;
+using Avalonia.Controls;
+using textban.Services;
+using textban.ViewModels;
+
+namespace textban.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
