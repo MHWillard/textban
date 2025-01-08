0dea77c46385da4f720aa5e028d825cd36e927a2
MHWillard <mhwillard@gmail.com>
2025-01-08 16:01:34 -0500

    UI work for scrollbar

---
+++ Services/ListManipulationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using textban.Models;
+using textban.ViewModels;
+
+namespace textban.Services
+{
+    public class ListManipulationService
+    {
+        public void moveItemToTaskList(ToDoItem TaskItem, ObservableCollection<ToDoItemViewModel> ToDoItems, ObservableCollection<ToDoItemViewModel> ReadyItems) { }
+    }
+}
--- tests/UnitTest.cs
+++ tests/UnitTest.cs
@@ -1,7 +1,11 @@
+using System.Collections.ObjectModel;
 using Avalonia.Controls;
 using Avalonia.Headless;
 using Avalonia.Headless.XUnit;
 using Xunit;
+using textban.Models;
+using textban.ViewModels;
+using textban.Services;
 
 namespace tests
 {
@@ -36,6 +40,33 @@ namespace tests
 
             // Assert:
             Assert.Equal("Hello World", textBox.Text);
+        }
+
+        [Fact]
+         public void ShouldMoveTaskItem()
+        {
+            //moveItemToTaskList, given two task lists for Backlog and Ready, item moved from Backlog should remove it and then appear in the Ready list
+            //Arrange
+            string TaskContent = "Walk dog";
+
+            var ToDoItems = new ObservableCollection<ToDoItemViewModel>(new[]
+            {
+            new ToDoItemViewModel() { Content = "Walk dog" }, new ToDoItemViewModel() { Content = "Mow lawn"}, new ToDoItemViewModel() { Content = "Mail package"}
+            });
+            
+            var ReadyItems = new ObservableCollection<ToDoItemViewModel>(new[]
+            {
+            new ToDoItemViewModel() { Content = "Floss teeth" }
+            });
+
+            ListManipulationService ListService = new ListManipulationService();
+
+            //Act
+            ListService.moveItemToTaskList(TaskContent, ToDoItems, ReadyItems);
+
+            //Assert
+            Assert.Contains(TaskContent, ReadyItems);
+            Assert.DoesNotContain(TaskContent, ToDoItems);
         }
     }
 }
